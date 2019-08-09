444c55d0b4ebbb86ddc3ecf40f5c751525444c32
Max Dymond <cmeister2@gmail.com>
2019-08-09 15:44:09 +0100

    Add a .travis.yml file for regressible checking of OSS-Fuzz

--- travisoss.sh
+++ travisoss.sh
@@ -7,15 +7,6 @@ PROJECT_NAME=aspell
 # Clone the oss-fuzz repository
 git clone https://github.com/google/oss-fuzz.git /tmp/ossfuzz
 
-if [[ ! -d /tmp/ossfuzz/projects/${PROJECT_NAME} ]]
-then
-    echo "Could not find the ${PROJECT_NAME} project in ossfuzz"
-
-    # Exit with a success code while the aspell project is not expected to exist
-    # on oss-fuzz.
-    exit 0
-fi
-
 # Work out which repo to clone from, inside Docker
 if [[ ${TRAVIS_PULL_REQUEST} != "false" ]]
 then
@@ -29,10 +20,11 @@ else
 fi
 
 # Modify the oss-fuzz Dockerfile so that we're checking out the current branch on travis.
-sed -i "s@https://github.com/GNUAspell/aspell-fuzz.git@-b ${BRANCH} https://github.com/${REPO}.git@" /tmp/ossfuzz/projects/${PROJECT_NAME}/Dockerfile
+sed -i "s@-b master https://github.com/gnuaspell/aspell-fuzz.git@-b ${BRANCH} https://github.com/${REPO}.git@" /tmp/ossfuzz/projects/${PROJECT_NAME}/Dockerfile
 
 # Try and build the fuzzers
 pushd /tmp/ossfuzz
 python infra/helper.py build_image --pull ${PROJECT_NAME}
 python infra/helper.py build_fuzzers ${PROJECT_NAME}
+python infra/helper.py check_build ${PROJECT_NAME}
 popd

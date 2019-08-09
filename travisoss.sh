f4ab57ffb4cede573711ada8c4a588cc54a45016
Max Dymond <cmeister2@gmail.com>
2019-08-09 18:33:29 +0100

    Test multiple builds using matrix

--- travisoss.sh
+++ travisoss.sh
@@ -4,6 +4,9 @@ set -ex
 
 PROJECT_NAME=aspell
 
+ENGINE=${1:-libfuzzer}
+SANITIZER=${2:-address}
+
 # Clone the oss-fuzz repository
 git clone https://github.com/google/oss-fuzz.git /tmp/ossfuzz
 
@@ -25,6 +28,12 @@ sed -i "s@-b master https://github.com/gnuaspell/aspell-fuzz.git@-b ${BRANCH} ht
 # Try and build the fuzzers
 pushd /tmp/ossfuzz
 python infra/helper.py build_image --pull ${PROJECT_NAME}
-python infra/helper.py build_fuzzers ${PROJECT_NAME}
-python infra/helper.py check_build ${PROJECT_NAME}
+python infra/helper.py build_fuzzers --engine=${ENGINE} --sanitizer=${SANITIZER} ${PROJECT_NAME}
+
+if [[ ${SANITIZER} != "coverage" ]]
+then
+    # Can't verify the coverage build, but can check all other types.
+    python infra/helper.py check_build --engine=${ENGINE} --sanitizer=${SANITIZER} ${PROJECT_NAME}
+fi
+
 popd
